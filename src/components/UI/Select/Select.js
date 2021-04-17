1bff9e86647161c5af0585b1476f0c9c7eea1978
Konstantin K <k.karavaev@platform.live>
2021-04-17 09:07:10 +0300

    create Select UI

---
+++ src/components/UI/Select/Select.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Select.module.css';
+
+const Select = props => {
+
+   return (
+      <div className={classes.Select}>
+         <label htmlFor=""></label>
+         <select></select>
+      </div>
+   );
+};
+
+export default Select;
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,9 +2,11 @@ import React from 'react';
 import classes from './Auth.module.css';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
+import is from 'is_js';
 
 export default class Auth extends React.Component {
    state = {
+      isFormValid: false,
       formControls: {
          email: {
             value: '',
@@ -45,17 +47,69 @@ export default class Auth extends React.Component {
       event.preventDefault();
    };
 
+   // валидация input (значение, правила)
+   validateControl(value, validation) {
+      if (!validation) {
+         return true;
+      }
+      let isValid = true;
+      // Проверка на обязательность поля
+      if (validation.required) {
+         isValid = value.trim() !== '' && isValid;
+      }
+      // проверка с помощью библиотеки is_js
+      if (validation.email) {
+         isValid = is.email(value) && isValid;
+      }
+
+      if (validation.minLength) {
+         isValid = value.length >= validation.minLength && isValid;
+      }
+
+      return isValid;
+   }
+
+   onChangeHandler = (event, controlName) => {
+      // создание копии formControl, чтобы не вызывать мутаций в state
+      const formControls = {...this.state.formControls};
+      const control = {...formControls[controlName]};
+
+      control.value = event.target.value;
+      control.touched = true;
+      control.valid = this.validateControl(control.value, control.validation);
+
+      formControls[controlName] = control;
+
+      let isFormValid = true;
+      Object.keys(formControls).forEach(name => {
+         isFormValid = formControls[name].valid && isFormValid;
+      });
+
+      this.setState({
+         formControls
+      });
+   };
+
    // динамическое формирование input из formControls с использованием шаблона UI/Input
    renderInputs() {
       // получить массив ключей
-      const inputs = Object.keys(this.state.formControls).map((controlName, index) => {
+      return Object.keys(this.state.formControls).map((controlName, index) => {
+         const control = this.state.formControls[controlName];
+
          return (
             <Input
-
+               key={controlName + index}
+               type={control.type}
+               value={control.value}
+               valid={control.valid}
+               touched={control.touched}
+               label={control.label}
+               shouldValidate={!!control.validation}
+               errorMessage={control.errorMessage}
+               onChange={(event) => this.onChangeHandler(event, controlName)}
             />
          );
       });
-      return inputs;
    }
 
    render() {
@@ -70,12 +124,14 @@ export default class Auth extends React.Component {
                   <Button
                      type="success"
                      onClick={this.loginHandler}
+                     disabled={!this.state.isFormValid}
                   >
                      Войти
                   </Button>
                   <Button
                      type="primary"
                      onClick={this.registerHandler}
+                     disabled={!this.state.isFormValid}
                   >
                      Зарегистрироваться
                   </Button>
